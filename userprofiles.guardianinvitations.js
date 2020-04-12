275257eab79c2bb8d9427552f4927d317531d882
Biswabijaya Samal <me@biswabijaya.com>
2020-04-13 02:51:44 +0530

    userprofiles.guardianinvitations final

--- userprofiles.guardianinvitations.js
+++ userprofiles.guardianinvitations.js
@@ -1,40 +1,32 @@
-//userprofiles.guardianinvitations - create
-function userProfilesGuardianInvitationsCreate(data) {
-    return gapi.client.classroom.userProfiles.guardianInvitations.create(data)
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+//userprofiles.guardianinvitations - list ::studentId compulsory in data
+function userProfilesGuardianInvitationsList(data) {
+  return gapi.client.classroom.userProfiles.guardianInvitations.list(data)
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
 
 //userprofiles.guardianinvitations - get
 function userProfilesGuardianInvitationsGet(studentId,invitationId) {
-    return gapi.client.classroom.userProfiles.guardianInvitations.get({
-      "studentId": studentId,
-      "invitationId": invitationId
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+  return gapi.client.classroom.userProfiles.guardianInvitations.get({
+    "studentId": studentId,
+    "invitationId": invitationId
+  })
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
 
-//userprofiles.guardianinvitations - list
-function userProfilesGuardianInvitationsList(studentId,invitedEmailAddress,pageSize,pageToken,states) {
-    return gapi.client.classroom.userProfiles.guardianInvitations.list({
-      "studentId": studentId,
-      "invitedEmailAddress": invitedEmailAddress,
-      "pageSize": pageSize,
-      "pageToken": pageToken,
-      "states": [
-        states
-      ]
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+//userprofiles.guardianinvitations - create
+function userProfilesGuardianInvitationsCreate(data) {
+  return gapi.client.classroom.userProfiles.guardianInvitations.create(data)
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
