80355a53e0abfba3bd11ddaf5c242da396ab9480
Biswabijaya Samal <me@biswabijaya.com>
2020-04-13 02:56:14 +0530

    userprofiles.guardians final

--- userprofiles.guardians.js
+++ userprofiles.guardians.js
@@ -1,38 +1,35 @@
-//userprofiles.guardians - delete
-function userProfilesGuardiansDelete(studentId,guardianId) {
-    return gapi.client.classroom.userProfiles.guardians.delete({
-      "studentId": studentId,
-      "guardianId": guardianId
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+//userprofiles.guardians - list :: studentId compulsory
+function userProfilesGuardiansList(data) {
+  return gapi.client.classroom.userProfiles.guardians.list(data)
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
+
 //userprofiles.guardians - get
 function userProfilesGuardiansGet(studentId,guardianId) {
-    return gapi.client.classroom.userProfiles.guardians.get({
-      "studentId": studentId,
-      "guardianId": guardianId
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-//userprofiles.guardians - list
-function userProfilesGuardiansList(studentId,invitedEmailAddress,pageSize,pageToken) {
-    return gapi.client.classroom.userProfiles.guardians.list({
-      "studentId": studentId,
-      "invitedEmailAddress": invitedEmailAddress,
-      "pageSize": pageSize,
-      "pageToken": pageToken
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+  return gapi.client.classroom.userProfiles.guardians.get({
+    "studentId": studentId,
+    "guardianId": guardianId
+  })
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
+
+//userprofiles.guardians - delete
+function userProfilesGuardiansDelete(studentId,guardianId) {
+  return gapi.client.classroom.userProfiles.guardians.delete({
+    "studentId": studentId,
+    "guardianId": guardianId
+  })
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
