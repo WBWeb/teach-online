e37d8f6bfdcbc9cc32fe7035e213bd77c0227c79
Biswabijaya Samal <me@biswabijaya.com>
2020-04-13 02:40:45 +0530

    userprofiles final

--- userprofiles.js
+++ userprofiles.js
@@ -1,11 +1,11 @@
 //userprofiles - get
 function userProfilesGet(userId) {
-    return gapi.client.classroom.userProfiles.get({
-      "userId": userId
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+  return gapi.client.classroom.userProfiles.get({
+    "userId": userId
+  })
+  .then(function(response) {
+    // Handle the results here (response.result has the parsed body).
+    console.log("Response", response);
+  },
+  function(err) { console.error("Execute error", err); });
+}
