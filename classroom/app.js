f1c338289cb5b7e388525392a57bcfdbf3883408
Biswabijaya Samal <me@biswabijaya.com>
2020-04-18 02:31:06 +0530

    Update app.js

--- classroom/app.js
+++ classroom/app.js
@@ -403,6 +403,7 @@ function modalAddCourse() {
             +'</select>'
           +'</div>'
         +'</div>'
+      +'</div>'
       +'<div class="row">'
         +'<div class="col-md-12">'
           +'<div class="form-group">'
